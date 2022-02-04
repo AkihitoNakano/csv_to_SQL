708a1aa75219723755a88038102c30ce0062f566
Akihito <linkin_087@yahoo.co.jp>
2022-02-05 02:13:23 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import csv
+
+
+csv_data = r'D:\03_personal\Program\Python\Bot\tweet_from_csv\list.csv'
+convert_text = 'tweet_list.txt'
+
+table_name = 'Proverb_list'
+
+def create_table(row):
+    return f'CREATE TABLE {table_name} (\n' \
+           f'index INT,\n' \
+           f'{row[0]} VARCHAR(140) NOT NULL,\n' \
+           f'{row[1]} VARCHAR(20) NOT NULL,\n' \
+           f'{row[2]} VARCHAR(10) NOT NULL,\n' \
+           f'{row[3]} INT,\n' \
+           f'{row[4]} INT,\n' \
+           f'{row[5]} DATE NOT NULL,\n' \
+           f'PRIMARY KEY(index),\n' \
+           f'):'
+
+def insert_values(i, row):
+    if  row[5] == '0' :
+        date = '0000-00-00'
+    else:
+        date = row[5].replace('/', '-')
+    return f'INSERT INT {table_name} VALUES\n' \
+           f'({i}, {row[0]}, {row[1]},{row[2]}, {row[3]}, {row[4]}, {date})\n'
+
+
+with open(csv_data, encoding='utf-8') as f:
+    reader = csv.reader(f)
+    values = ''
+    for i, row in enumerate(reader):
+        if i == 0:
+            table = create_table(row)
+        else:
+            values += insert_values(i, row)
+
+with open(convert_text, mode='wt', encoding='utf-8') as f:
+    f.write(f'{table} \n\n{values}')
+
+
+
+print(f'{table} + \n+ {values}')
