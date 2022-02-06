5e7dc7a13081455b9cd09f9d37c4916f7006adff
Akihito <linkin_087@yahoo.co.jp>
2022-02-07 02:44:09 +0900

    changed syntax error and table name.

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import csv
 
 
-csv_data = r'D:\03_personal\Program\Python\Bot\tweet_from_csv\list.csv'
+csv_data = 'list.csv'
 convert_text = 'tweet_list.txt'
 
-table_name = 'Proverb_list'
+table_name = 'list'
 
 def create_table(row):
-    return f'CREATE TABLE {table_name} (\n' \
-           f'index INT,\n' \
+    return f'CREATE TABLE {table_name}(\n' \
+           f'id INT,\n' \
            f'{row[0]} VARCHAR(140) NOT NULL,\n' \
            f'{row[1]} VARCHAR(20) NOT NULL,\n' \
            f'{row[2]} VARCHAR(10) NOT NULL,\n' \
            f'{row[3]} INT,\n' \
            f'{row[4]} INT,\n' \
            f'{row[5]} DATE NOT NULL,\n' \
-           f'PRIMARY KEY(index),\n' \
-           f'):'
+           f'PRIMARY KEY(id)\n' \
+           f');'
 
 def insert_values(i, row):
     if  row[5] == '0' :
         date = '0000-00-00'
     else:
         date = row[5].replace('/', '-')
-    return f'INSERT INT {table_name} VALUES\n' \
-           f'({i}, {row[0]}, {row[1]},{row[2]}, {row[3]}, {row[4]}, {date})\n'
+
+    return f'INSERT INTO {table_name} VALUES\n' \
+           f'({i}, "{row[0]}", "{row[1]}","{row[2]}", {row[3]}, {row[4]}, "{date}");\n'
 
 
 with open(csv_data, encoding='utf-8') as f:
@@ -41,4 +42,4 @@ with open(convert_text, mode='wt', encoding='utf-8') as f:
 
 
 
-print(f'{table} + \n+ {values}')
+# print(f'{table} + \n+ {values}')
